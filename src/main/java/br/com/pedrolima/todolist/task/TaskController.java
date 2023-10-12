5463ec7ef77cd89aae19d8d003f0c129b481abf2
Pedro Augusto Pinto de Lima <pedro.plima@csu.com.br>
2023-10-12 16:45:51 -0300

    feat: validações de data na criação das tasks

--- src/main/java/br/com/pedrolima/todolist/task/TaskController.java
+++ src/main/java/br/com/pedrolima/todolist/task/TaskController.java
@@ -1,10 +1,17 @@
 package br.com.pedrolima.todolist.task;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/tasks")
@@ -13,10 +20,21 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel Create(@RequestBody TaskModel taskModel) {
-        var taskCreated = taskRepository.save(taskModel);
+    public ResponseEntity Create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+        UUID idUser = (UUID) request.getAttribute("idUser");
+        taskModel.setIdUser(idUser);
+        var currentDate = LocalDateTime.now();
+        if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("A data de início / data de término deve ser maior do que a data atual");
+        }
 
-        return taskCreated;
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("A data de início deve ser menor que a data de término");
+        }
 
+        TaskModel taskCreated = taskRepository.save(taskModel);
+        return ResponseEntity.status(HttpStatus.OK).body(taskCreated);
     }
 }
